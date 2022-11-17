b9d456bd037a34a2e9d4df66f81bcfd195888e2d
Frecherenkel60 <rieger.kevin01@gmail.com>
2022-11-17 14:01:50 +0100

    Initialize Backend (walking skeleton, no configuration yet -> not running)

---
+++ src/main/java/com/wwi21sebgroup5/cinema/CinemaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wwi21sebgroup5.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wwi21sebgroup5/cinema/CinemaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwi21sebgroup5.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
