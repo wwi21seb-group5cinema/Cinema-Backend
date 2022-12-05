a941f07fcf50159904ab41279dfc0ca0f5dbf3ff
Frecherenkel60 <rieger.kevin01@gmail.com>
2022-12-06 00:04:09 +0100

    added entities, not working yet because of h2

--- src/main/java/com/wwi21sebgroup5/cinema/entities/User.java
+++ src/main/java/com/wwi21sebgroup5/cinema/entities/User.java
@@ -1,4 +1,61 @@
 package com.wwi21sebgroup5.cinema.entities;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "User")
 public class User {
+    @Id
+    @Column
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column
+    @NotNull
+    private String userName;
+
+    @Column
+    @NotNull
+    private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", referencedColumnName = "id")
+    private Role role;
+
+    @Column
+    @NotNull
+    private String firstName;
+
+    @Column
+    @NotNull
+    private String lastName;
+
+    @Column
+    @NotNull
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "city_id", referencedColumnName = "id")
+    private City city;
+
+    @Column
+    @NotNull
+    private String street;
+
+    @Column
+    @NotNull
+    private String number;
+
 }
