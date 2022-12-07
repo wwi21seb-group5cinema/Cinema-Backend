1250dfa5ef6d435923407218a98fae71b8b59c86
Frecherenkel60 <rieger.kevin01@gmail.com>
2022-12-07 14:43:02 +0100

    sync

--- src/main/java/com/wwi21sebgroup5/cinema/config/SecurityConfig.java
+++ src/main/java/com/wwi21sebgroup5/cinema/config/SecurityConfig.java
@@ -2,8 +2,15 @@ package com.wwi21sebgroup5.cinema.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,16 +20,41 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf ->
-                        csrf.disable()
+                .csrf(AbstractHttpConfigurer::disable)
+                .securityMatcher("/v1/**")
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**").hasRole("USER")
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .authorizeHttpRequests(requests ->
-                        requests.requestMatchers("/v1/**").permitAll()
-                                .anyRequest().authenticated()
+                .logout(logout -> logout
+                        .invalidateHttpSession(true)
                 )
-                .httpBasic();
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("user")
+                        .password(passwordEncoder().encode("password"))
+                        .roles("USER")
+                        .build(),
+                User.builder()
+                        .username("admin")
+                        .password(passwordEncoder().encode("password"))
+                        .roles("ADMIN")
+                        .build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
