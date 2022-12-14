62c8c8f2a26fa2f6f13cef58a16f22de1d43a7ad
Frecherenkel60 <rieger.kevin01@gmail.com>
2022-12-14 09:16:08 +0100

    initialized remaining entities

--- src/main/java/com/wwi21sebgroup5/cinema/entities/Producer.java
+++ src/main/java/com/wwi21sebgroup5/cinema/entities/Producer.java
@@ -1,4 +1,26 @@
 package com.wwi21sebgroup5.cinema.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "producer")
 public class Producer {
+
+    @Id
+    @Column
+    @NotNull
+    private UUID id;
+    
 }
--- src/main/java/com/wwi21sebgroup5/cinema/entities/Seat.java
+++ src/main/java/com/wwi21sebgroup5/cinema/entities/Seat.java
@@ -1,4 +1,25 @@
 package com.wwi21sebgroup5.cinema.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "seat")
 public class Seat {
+    @Id
+    @Column
+    @NotNull
+    private UUID id;
+
 }
