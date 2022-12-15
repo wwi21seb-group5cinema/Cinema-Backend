77029c1e21844826eb2198a317a210f2c5ca1db8
Frecherenkel60 <rieger.kevin01@gmail.com>
2022-12-15 12:32:08 +0100

    removed manual getter and setter for genre

--- src/main/java/com/wwi21sebgroup5/cinema/entities/Genre.java
+++ src/main/java/com/wwi21sebgroup5/cinema/entities/Genre.java
@@ -19,11 +19,4 @@ public class Genre {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
