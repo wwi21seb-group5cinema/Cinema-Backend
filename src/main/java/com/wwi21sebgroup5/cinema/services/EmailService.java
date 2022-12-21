6cce0c2c5da7195031e531915e1649df4457cc61
Frecherenkel60 <rieger.kevin01@gmail.com>
2022-12-21 18:14:05 +0100

    sync

---
+++ src/main/java/com/wwi21sebgroup5/cinema/services/EmailService.java
@@ -0,0 +1,66 @@
+package com.wwi21sebgroup5.cinema.services;
+
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    public void sendMail(String recipient, String subject, String msgBody) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        try {
+            mailMessage.setFrom(sender);
+            mailMessage.setTo(recipient);
+            mailMessage.setText(msgBody);
+            mailMessage.setSubject(subject);
+
+            javaMailSender.send(mailMessage);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void sendMail(String recipient, String subject, String msgBody, String attachment) {
+        MimeMessage mimeMessage
+                = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper;
+
+        try {
+            mimeMessageHelper
+                    = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setFrom(sender);
+            mimeMessageHelper.setTo(recipient);
+            mimeMessageHelper.setText(msgBody);
+            mimeMessageHelper.setSubject(subject);
+
+            // Adding the attachment
+            FileSystemResource file
+                    = new FileSystemResource(
+                    new File(attachment));
+
+            mimeMessageHelper.addAttachment(
+                    file.getFilename(), file);
+
+            // Sending the mail
+            javaMailSender.send(mimeMessage);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
