f3e61cef098d488a709e54fc440ca12500ec8769
Frecherenkel60 <rieger.kevin01@gmail.com>
2022-12-29 01:17:43 +0100

    added tests for cinema service

--- src/test/java/com/wwi21sebgroup5/cinema/services/CinemaServiceTest.java
+++ src/test/java/com/wwi21sebgroup5/cinema/services/CinemaServiceTest.java
@@ -3,7 +3,10 @@ package com.wwi21sebgroup5.cinema.services;
 import com.wwi21sebgroup5.cinema.entities.Cinema;
 import com.wwi21sebgroup5.cinema.entities.CinemaHall;
 import com.wwi21sebgroup5.cinema.entities.City;
+import com.wwi21sebgroup5.cinema.exceptions.CinemaAlreadyExistsException;
 import com.wwi21sebgroup5.cinema.repositories.CinemaRepository;
+import com.wwi21sebgroup5.cinema.requestObjects.CinemaRequestObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,11 +18,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CinemaServiceTest {
+
+    @Mock
+    CityService cityService;
 
     @Mock
     CinemaRepository cinemaRepository;
@@ -65,6 +71,55 @@ public class CinemaServiceTest {
 
         Optional<Cinema> actualCinema = cinemaService.getCinemaById(id);
         assertEquals(expectedCinema, actualCinema.get());
+    }
+
+    @Test
+    @DisplayName("Test adding cinema successfully")
+    public void testAddCinemaSuccessful() {
+        String name = "testName", plz = "68259", cityName = "Wallstadt", street = "TestStreet", houseNumber = "28";
+        int floors = 3;
+
+        City city = new City(plz, cityName);
+        CinemaRequestObject requestObject = new CinemaRequestObject(
+                name, plz, cityName, street, houseNumber, floors
+        );
+        Cinema expectedCinema = new Cinema(
+                name, List.of(), city, street, houseNumber, floors
+        );
+
+        when(cityService.findByPlzAndName(plz, cityName)).thenReturn(city);
+        when(cinemaRepository.findByCityAndStreetAndHouseNumber(city, street, houseNumber))
+                .thenReturn(Optional.empty());
+        when(cinemaRepository.save(expectedCinema)).thenReturn(expectedCinema);
+
+        Cinema actualCinema = null;
+        try {
+            actualCinema = cinemaService.add(requestObject);
+        } catch (CinemaAlreadyExistsException e) {
+            fail();
+        }
+        Assertions.assertEquals(expectedCinema, actualCinema);
+    }
+
+    @Test
+    @DisplayName("Test cinema already exists during add process")
+    public void testCinemaAlreadyExistsDuringAddProcess() {
+        String name = "testName", plz = "68259", cityName = "Wallstadt", street = "TestStreet", houseNumber = "28";
+        int floors = 3;
+
+        City city = new City(plz, cityName);
+        CinemaRequestObject requestObject = new CinemaRequestObject(
+                name, plz, cityName, street, houseNumber, floors
+        );
+        Cinema existingCinema = new Cinema(
+                name, List.of(), city, street, houseNumber, floors
+        );
+
+        when(cityService.findByPlzAndName(plz, cityName)).thenReturn(city);
+        when(cinemaRepository.findByCityAndStreetAndHouseNumber(city, street, houseNumber))
+                .thenReturn(Optional.of(existingCinema));
+
+        assertThrows(CinemaAlreadyExistsException.class, () -> cinemaService.add(requestObject));
     }
 
 }
