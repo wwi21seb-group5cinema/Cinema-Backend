16a82ff12be9c38e6d4b523a4d45251a537f7f5a
Frecherenkel60 <rieger.kevin01@gmail.com>
2023-01-10 16:10:07 +0100

    add custom bean to process thymeleaf-templates

--- src/main/java/com/wwi21sebgroup5/cinema/config/MailConfig.java
+++ src/main/java/com/wwi21sebgroup5/cinema/config/MailConfig.java
@@ -1,44 +1,31 @@
 package com.wwi21sebgroup5.cinema.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
 public class MailConfig {
 
-    private final String authLiteral = "spring.mail.properties.mail.smtp.auth";
-    private final String startTlsLiteral = "spring.mail.properties.mail.smtp.starttls.enable";
-    @Value("${spring.mail.host}")
-    private String host;
-    @Value("${spring.mail.port}")
-    private int port;
-    @Value("${spring.mail.username}")
-    private String username;
-    @Value("${spring.mail.password}")
-    private String password;
-    @Value("${spring.mail.properties.mail.smtp.auth}")
-    private boolean auth;
-
-    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
-    private boolean startTls;
-/*
     @Bean
-    public JavaMailSender javaMailSender() {
-        final JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    @Primary
+    public ITemplateResolver thymeleafTemplateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML");
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
 
-        // basic config
-        mailSender.setHost(host);
-        mailSender.setPort(port);
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
-
-        // specific java-mail config
-        final Properties javaMailProps = new Properties();
-        javaMailProps.put(authLiteral, auth);
-        javaMailProps.put(startTlsLiteral, startTls);
-        mailSender.setJavaMailProperties(javaMailProps);
-
-        return mailSender;
-    }*/
+    @Bean
+    public SpringTemplateEngine thymeleafTemplateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
 
 }
