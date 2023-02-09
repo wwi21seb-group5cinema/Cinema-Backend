ea900da71d859aa7ac9ee8a6f1285253eb8da7aa
Frecherenkel60 <rieger.kevin01@gmail.com>
2023-02-09 19:32:27 +0100

    hotfix

--- src/main/java/com/wwi21sebgroup5/cinema/entities/Movie.java
+++ src/main/java/com/wwi21sebgroup5/cinema/entities/Movie.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDate;
 import java.util.Objects;
@@ -68,8 +67,7 @@ public class Movie {
     private String name;
 
     @NotNull
-    @Column
-    @Length(max = 1000)
+    @Column(length = 1500)
     private String description;
 
     @NotNull
--- src/main/java/com/wwi21sebgroup5/cinema/repositories/MovieRepository.java
+++ src/main/java/com/wwi21sebgroup5/cinema/repositories/MovieRepository.java
@@ -4,11 +4,14 @@ import com.wwi21sebgroup5.cinema.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MovieRepository extends JpaRepository<Movie, UUID> {
 
     @Override
     List<Movie> findAll();
+
+    Optional<Movie> findByName(String name);
 
 }
